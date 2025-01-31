2184242a0704a24210f669c30abacc9d36cd8474
7dong7 <tmdkdl777@gmail.com>
2025-01-31 21:06:08 +0900

    Collection Framework - Linked List 와 Array List 의 요소에 대한 접근 시간 테스트 / stack 사용 예시

---
+++ src/main/java/org/collectionFrameWork/stackAndQueue/StackAndQueue.java
@@ -0,0 +1,30 @@
+package org.collectionFrameWork.stackAndQueue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class StackAndQueue {
+    public static void main(String[] args) {
+        Stack st = new Stack();
+        Queue q = new LinkedList(); // queue 의 구현체 LinkedList 를 사용해서 구현
+
+        st.push("0");
+        st.push("1");
+        st.push("2");
+
+        q.offer("0");
+        q.offer("1");
+        q.offer("2");
+
+        System.out.println("=== stack ===");
+        while (!st.isEmpty()) {
+            System.out.println(st.pop());
+        }
+
+        System.out.println("=== queue ===");
+        while (!q.isEmpty()) {
+            System.out.println(q.poll());
+        }
+    }
+}
---
+++ src/main/java/org/collectionFrameWork/stackAndQueue/StackEx1.java
@@ -0,0 +1,72 @@
+package org.collectionFrameWork.stackAndQueue;
+
+import java.util.Stack;
+
+// 스택 활용
+public class StackEx1 {
+    public static Stack back = new Stack(); // 사이트를 방문할 때마다 이전페이지를 기록하는 스택
+    public static Stack forward = new Stack(); // '뒤로' 버튼을 눌렀을 경우 생기는 스택
+
+    public static void main(String[] args) {
+        goURL("1.네이트"); // '네이트' 페이지 방문
+        goURL("2.야후");
+        goURL("3.네이버");
+        goURL("4.다음");
+
+        printStatus();
+
+        goBack();
+        System.out.println("= '뒤로' 버튼을 누른후 =");
+        printStatus();
+
+        goBack();
+        System.out.println("= '뒤로' 버튼을 누른후 =");
+        printStatus();
+
+        goForward();
+        System.out.println("= '앞으로' 버튼을 누른후 =");
+        printStatus();
+
+        goForward();
+        System.out.println("= '앞으로' 버튼을 누른후 =");
+        printStatus();
+
+        goURL("codechobo.com");
+        System.out.println("= 새로운 주소로 이동 후 =");
+        printStatus();
+
+        goBack();
+        printStatus();
+    }
+
+    public static void printStatus() {
+        System.out.println("back: " + back);
+        System.out.println("forward: " + forward);
+        System.out.println("현재화면은 '" + back.peek()+"' 입니다");
+        System.out.println();
+    }
+
+    public static void goURL(String url) { // 페이지에 방문하는 메소드
+        back.push(url); // 현재 방문한 페이지를 스택에 쌓는다
+        if(!forward.isEmpty()) { // 새로운 사이트를 방문했기 때문에 현재 페이지가 스택의 가장 위에 있다  
+            forward.clear(); // '앞으로'버튼으로 방문할 페이지는 없다
+        }
+    }
+
+    public static void goForward() { // '앞으로'
+        if(!back.isEmpty()) {
+            back.push(forward.pop());
+            /*
+                '앞으로' 버튼을 누르면 
+                forward 에서 현재 페이지를 제거하고
+                제거한 값을 back 에 다시 넣는다
+             */
+        }
+    }
+
+    public static void goBack() { // '뒤로'
+        if(!back.isEmpty()) {
+            forward.push(back.pop());
+        }
+    }
+}
