e55db69536d7784389f67e8abe6d8d06d23d4e7d
7dong7 <tmdkdl777@gmail.com>
2025-01-31 22:15:41 +0900

    Collection Framework - HashSet 사용

---
+++ src/main/java/org/collectionFrameWork/hashSetEx/HashSetEx.java
@@ -0,0 +1,18 @@
+package org.collectionFrameWork.hashSetEx;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetEx {
+    public static void main(String[] args) {
+        Object[] objArr = {"1", 1, "2", "2", "3", "3", "4", "4", "4"};
+        Set set = new HashSet<>();
+
+        for (int i = 0; i < objArr.length; i++) {
+            set.add(objArr[i]); // HashSet 에 objArr 의 요소들을 저장
+        }
+        // HashSet 에 저장된 요소들을 출력한다
+        System.out.println("set = " + set); // set = [1, 1, 2, 3, 4]
+        // "1", 1 은 다르기 때문에 중복이 아니다
+    }
+}
---
+++ src/main/java/org/collectionFrameWork/hashSetEx/HashSetLotto.java
@@ -0,0 +1,17 @@
+package org.collectionFrameWork.hashSetEx;
+
+import java.util.*;
+
+public class HashSetLotto {
+    public static void main(String[] args) {
+        Set set = new HashSet<>();
+
+        for ( int i = 0; set.size() < 6; i++ ) {
+            int num = (int) (Math.random() * 45) + 1;
+            set.add(num);
+        }
+        List list = new LinkedList<>(set); // Linked List( Collection c)
+        Collections.sort(list);
+        System.out.println("list = " + list); // list = [2, 9, 18, 26, 33, 35]
+    }
+}
